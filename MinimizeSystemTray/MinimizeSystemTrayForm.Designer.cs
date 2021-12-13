47f2551b0736b5e0ef826c8b878ab4254d96f2b6
指月指 <1286637198@qq.com>
2021-12-13 14:01:53 +0800

    添加项目文件。

---
+++ MinimizeSystemTray/MinimizeSystemTrayForm.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace MinimizeSystemTray
+{
+    partial class MinimizeSystemTrayForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MinimizeSystemTrayForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.exit = new System.Windows.Forms.ToolStripMenuItem();
+            this.showForm = new System.Windows.Forms.ToolStripMenuItem();
+            this.notifyCtxMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.notifyCtxMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 16F);
+            this.label1.Location = new System.Drawing.Point(182, 190);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(406, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "我是点击关闭会最小化到系统托盘的程序";
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Visible = true;
+            // 
+            // exit
+            // 
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(100, 22);
+            this.exit.Text = "退出";
+            // 
+            // showForm
+            // 
+            this.showForm.Name = "showForm";
+            this.showForm.Size = new System.Drawing.Size(100, 22);
+            this.showForm.Text = "显示";
+            // 
+            // notifyCtxMenuStrip
+            // 
+            this.notifyCtxMenuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.notifyCtxMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exit,
+            this.showForm});
+            this.notifyCtxMenuStrip.Name = "contextMenuStrip1";
+            this.notifyCtxMenuStrip.Size = new System.Drawing.Size(101, 48);
+            // 
+            // MinimizeSystemTrayForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Name = "MinimizeSystemTrayForm";
+            this.Text = "MinimizeSystemTrayForm";
+            this.Load += new System.EventHandler(this.MinimizeSystemTrayForm_Load);
+            this.notifyCtxMenuStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.ToolStripMenuItem exit;
+        private System.Windows.Forms.ToolStripMenuItem showForm;
+        private System.Windows.Forms.ContextMenuStrip notifyCtxMenuStrip;
+    }
+}
+
---
+++ MinimizeSystemTray/MinimizeSystemTrayForm.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MinimizeSystemTray
+{
+    public partial class MinimizeSystemTrayForm : Form
+    {
+        public MinimizeSystemTrayForm()
+        {
+            InitializeComponent();
+
+            #region 最小化到任务托盘所有的处理
+            FormClosing += MainForm_FormClosing;
+            SizeChanged += MainForm_SizeChanged;
+
+            notifyIcon.Text = "MyApp-" + Text;
+            notifyIcon.MouseDoubleClick += ShowNomalFormEvent;
+            notifyIcon.ContextMenuStrip = notifyCtxMenuStrip;
+            notifyCtxMenuStrip.Items["exit"].Click += Notity_ContextMenu_Exit_Click;
+            notifyCtxMenuStrip.Items["showForm"].Click += ShowNomalFormEvent;
+            #endregion
+            
+        }
+
+        #region 最小化到任务托盘所有的处理
+        /// <summary>
+        /// 主窗口关闭，最小化到任务托盘
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //取消关闭，进入任务托盘
+            if (e.CloseReason == CloseReason.UserClosing)//判断关闭事件来源 当通过点击窗口的关闭按钮时最小化到任务托盘
+            {
+                e.Cancel = true;
+                // this.WindowState = FormWindowState.Minimized;
+                Hide();
+
+                ShowInTaskbar = false;
+            }
+        }
+        /// <summary>
+        /// 主窗口大小变化
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MainForm_SizeChanged(object sender, EventArgs e)
+        {
+            //判断是否是最小化，隐藏ShowInTaskbar，否则从ShowInTaskbar关闭将无法恢复显示
+            if (WindowState == FormWindowState.Minimized)
+            {
+                ShowInTaskbar = false;
+            }
+        }
+
+        /// <summary>
+        /// 右键任务托盘 退出
+        /// </summary>
+        private void Notity_ContextMenu_Exit_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("是否确认退出?", "退出", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+            {
+                //必须先释放资源，然后Close关闭，否则会会触发FormClosing事件(因为FormClosing事件方法中已经取消关闭)。
+                //使用 Application.Exit必须判断关闭事件的来源，窗口的关闭取消
+                Application.Exit();
+                //释放资源
+                //Dispose();
+                //Close();
+            }
+        }
+
+        /// <summary>
+        /// 双击托盘的图标和右键菜单 显示窗口
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ShowNomalFormEvent(object sender, EventArgs e)
+        {
+            ShowNomalForm();
+        }
+
+        //显示主界面
+        void ShowNomalForm()
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                WindowState = FormWindowState.Normal;
+            }
+
+            //任务栏区显示图标。必须设置任务栏图标显示，否则窗口可能不显示
+            ShowInTaskbar = true;
+            Show();
+            //激活窗体并给予焦点
+            Activate();
+        }
+        #endregion
+
+        private void MinimizeSystemTrayForm_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("1");
+        }
+    }
+}
