2f354651f43e1ac12def97fec15ef9aa8f2fae3a
指月指 <1286637198@qq.com>
2022-08-24 15:34:04 +0800

    20220824 本地提示框和(气泡)提示框

--- NotificationTrayToolTip/Form1.Designer.cs
+++ NotificationTrayToolTip/Form1.Designer.cs
@@ -30,20 +30,43 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem(new string[] {
+            "ListViewItem1",
+            "one",
+            "Two"}, -1);
             this.buttonPro1 = new CMControls.ButtonPro();
             this.buttonPro2 = new CMControls.ButtonPro();
             this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuStrip按钮ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // buttonPro1
             // 
+            this.buttonPro1.BackColor = System.Drawing.Color.Transparent;
             this.buttonPro1.FlatAppearance.BorderSize = 0;
             this.buttonPro1.FlatAppearance.CheckedBackColor = System.Drawing.Color.Transparent;
             this.buttonPro1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
             this.buttonPro1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
             this.buttonPro1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonPro1.ForeColor = System.Drawing.Color.White;
-            this.buttonPro1.Location = new System.Drawing.Point(12, 40);
+            this.buttonPro1.Location = new System.Drawing.Point(12, 54);
             this.buttonPro1.Name = "buttonPro1";
             this.buttonPro1.Size = new System.Drawing.Size(194, 47);
             this.buttonPro1.TabIndex = 0;
@@ -53,13 +76,14 @@
             // 
             // buttonPro2
             // 
+            this.buttonPro2.BackColor = System.Drawing.Color.Transparent;
             this.buttonPro2.FlatAppearance.BorderSize = 0;
             this.buttonPro2.FlatAppearance.CheckedBackColor = System.Drawing.Color.Transparent;
             this.buttonPro2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
             this.buttonPro2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
             this.buttonPro2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonPro2.ForeColor = System.Drawing.Color.White;
-            this.buttonPro2.Location = new System.Drawing.Point(256, 40);
+            this.buttonPro2.Location = new System.Drawing.Point(256, 54);
             this.buttonPro2.Name = "buttonPro2";
             this.buttonPro2.RoundNormalColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(30)))), ((int)(((byte)(120)))));
             this.buttonPro2.Size = new System.Drawing.Size(194, 47);
@@ -72,18 +96,153 @@
             // 
             this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
             this.notifyIcon.Text = "notifyIcon1";
-            this.notifyIcon.Visible = true;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(29, 120);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(264, 57);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(256, 31);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(256, 31);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 25);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(505, 25);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(128, 22);
+            this.toolStripLabel1.Text = "ToolStrip控件的Label";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuStrip按钮ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(505, 25);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuStrip按钮ToolStripMenuItem
+            // 
+            this.menuStrip按钮ToolStripMenuItem.Name = "menuStrip按钮ToolStripMenuItem";
+            this.menuStrip按钮ToolStripMenuItem.Size = new System.Drawing.Size(104, 21);
+            this.menuStrip按钮ToolStripMenuItem.Text = "MenuStrip按钮";
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            listViewItem2.Checked = true;
+            listViewItem2.StateImageIndex = 1;
+            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem2});
+            this.listView1.Location = new System.Drawing.Point(12, 194);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(194, 97);
+            this.listView1.TabIndex = 5;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1});
+            this.dataGridView1.Location = new System.Drawing.Point(256, 215);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(234, 110);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 319);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(111, 322);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(95, 16);
+            this.radioButton1.TabIndex = 8;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(276, 354);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "提示框大小";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(483, 135);
+            this.ClientSize = new System.Drawing.Size(505, 404);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.buttonPro2);
             this.Controls.Add(this.buttonPro1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -92,6 +251,20 @@
         private CMControls.ButtonPro buttonPro1;
         private CMControls.ButtonPro buttonPro2;
         private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.ToolTip toolTip;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuStrip按钮ToolStripMenuItem;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- NotificationTrayToolTip/Form1.cs
+++ NotificationTrayToolTip/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace NotificationTrayToolTip
 {
@@ -16,7 +17,74 @@ namespace NotificationTrayToolTip
         {
             InitializeComponent();
             notifyIcon.Visible = false;
-            
+
+            //必须设置ShowToolTips为true，当鼠标位于tab时显示提示
+            tabControl1.ShowToolTips = true;
+            tabPage1.ToolTipText = "你好，我是tab提示";
+
+
+            dataGridView1.ShowCellToolTips = true;
+            dataGridView1[0, 0].ToolTipText = "单元格的tip";
+
+
+            toolStrip1.ShowItemToolTips = true;
+            toolStripLabel1.AutoToolTip = true;
+            toolStripLabel1.ToolTipText = "toolStrip1子项的提示";
+
+            menuStrip1.ShowItemToolTips = true;
+            menuStrip按钮ToolStripMenuItem.AutoToolTip = true;
+            menuStrip按钮ToolStripMenuItem.ToolTipText = "menuStrip1子项的提示";
+
+            listView1.ShowItemToolTips = true;
+            listView1.Items[0].ToolTipText = "我是listViewItem的提示";
+
+            toolTip.SetToolTip(button1, "我是按钮1提示");
+            toolTip.SetToolTip(radioButton1, "单选提示，必需选择");
+            toolTip.SetToolTip(buttonPro1, "使用ToolTip的提示");
+
+
+            toolTip.AutomaticDelay = 300;
+            toolTip.AutoPopDelay = 10000;
+
+            //toolTip.ToolTipIcon = ToolTipIcon.Error;
+            //toolTip.ToolTipTitle = "错误提示！";
+
+            // 气泡提示框
+            toolTip.IsBalloon = true;
+
+            // 父控件不激活也显示
+            toolTip.ShowAlways = true;
+
+            button1.KeyUp += Button1_KeyDown;
+            button1.MouseEnter += Button1_MouseEnter;
+
+
+            toolTip.SetToolTip(label1, "我是一段内容非常长的提示框信息，\r\n主要是测试消息提示框的大小修改或者\r\n宽度需改的情况，可以做到根据消息\r\n多少很好的显示消息框");
+            toolTip.Popup += ToolTip_Popup;
+        }
+
+        private void ToolTip_Popup(object sender, PopupEventArgs e)
+        {
+            if (e.AssociatedControl==label1)
+            {
+                e.ToolTipSize = new Size(230, 90);
+            }
+        }
+
+        private void Button1_MouseEnter(object sender, EventArgs e)
+        {
+            toolTip.SetToolTip(button1, "鼠标进入提示");
+        }
+
+        private void Button1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode== Keys.Enter)
+            {
+                //toolTip.Hide(button1);
+                //toolTip.Show("密码不能为空，邮箱格式不正确等", button1); // 一直显示
+                //toolTip.Show("密码不能为空，只显示5秒", button1,5000); // 显示5000毫秒
+                toolTip.Show("密码不能为空，在上方显示", button1, 0, -button1.Height, 5000);
+            }
         }
 
         private void buttonPro1_Click(object sender, EventArgs e)
