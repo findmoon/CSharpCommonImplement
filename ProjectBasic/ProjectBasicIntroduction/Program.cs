af11f6a29e1e33627db748b22877e5a446b85ce5
指月指 <1286637198@qq.com>
2022-12-12 14:16:42 +0800

    202212121

---
+++ ProjectBasic/ProjectBasicIntroduction/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("ProjectBasic 分类下的项目说明!");
+Console.WriteLine("本部分作为一些通用的项目基础使用，类似项目可直接在此基础上开发使用");
+Console.WriteLine("---------------");
+Console.WriteLine("PGDBManagement 项目 - PostgreSQL静默安装的管理工具，包含对cmd执行、cmd命令交互执行、C#原生压缩和解压缩、长文件名处理、Windows服务的管理、简单进度条的自定义等...");
+
+
+Console.ReadLine();
