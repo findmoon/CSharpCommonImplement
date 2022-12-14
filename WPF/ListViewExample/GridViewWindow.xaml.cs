de2da8497f4f193fd88bb91d4502dd9c3d5a5bdd
指月指 <1286637198@qq.com>
2022-12-14 19:08:35 +0800

    20221214

--- WPF/ListViewExample/GridViewWindow.xaml.cs
+++ WPF/ListViewExample/GridViewWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -22,6 +23,16 @@ namespace ListViewExample
         public GridViewWindow()
         {
             InitializeComponent();
+
+            // 右键菜单的位置
+            //listViewContextMenu.Placement = PlacementMode.Right | PlacementMode.Bottom;
+            //listViewContextMenu.HorizontalOffset = 0;
+            //listViewContextMenu.VerticalOffset = 0;
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            
         }
     }
 }
