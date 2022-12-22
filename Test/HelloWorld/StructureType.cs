a037980e05b488810ab4a04759bdcc5e66ea7871
指月指 <1286637198@qq.com>
2022-12-22 23:19:30 +0800

    20221222

---
+++ Test/HelloWorld/StructureType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal struct StructureType
+    {
+        internal readonly int Id { get; init; }
+        internal string Name { get; set; }
+
+        public StructureType()
+        {
+            Id = 10;
+            Name = "abc";
+        }
+    }
+
+    internal struct StructureType1
+    {
+        internal int Id { get; set; }
+
+
+    }
+}
